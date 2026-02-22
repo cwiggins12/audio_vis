126432aca20e57061177e5dc6c496750463a4c0d
cody <codywiggins2112@gmail.com>
2026-02-22 11:41:54 -0600

    refactor to only 1 cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
+#define MINIAUDIO_IMPLEMENTATION
+
 #include "audio_capture.h"
 #include "analysis.h"
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <algorithm>
 
 int main() {
     AudioCapture capture;
 
     const ma_uint32 framesPerBuffer = 512;
+    const ma_uint32 fft_size = 2048;
 
     if (!capture.init(framesPerBuffer)) {
         std::cerr << "Failed to initialize audio capture.\n";
@@ -18,30 +16,12 @@ int main() {
 
     const ma_uint32 channels = capture.getChannels();
     
-    std::vector<Peak> peaks;
-    std::vector<RMS> rmss;
-    peaks.resize(channels);
-    rmss.resize(channels);
+    std::vector<Peak> peaks(channels);
+    std::vector<RMS> rmss(channels);
     //NOTE: remember to mono sum BEFORE PUSHING TO THIS OBJECT
-    FFT fft(framesPerBuffer * channels);
+    FFT fft(fft_size);
 
-    std::cout << "Capturing for 1 second...\n";
-    std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    // Decide how many FRAMES you want
-    const ma_uint32 framesToRead = framesPerBuffer * 4;
-    // Allocate frames * channels
-    std::vector<float> snapshot(framesToRead * channels);
-
-    // Pass frame count only
-    ma_uint32 lastWriteIndex = capture.getBlock(snapshot.data(), framesToRead);
-
-    std::cout << "Last write index: " << lastWriteIndex << "\n";
-
-    std::cout << "First 10 samples:\n";
-    for (ma_uint32 i = 0; i < std::min((ma_uint32)snapshot.size(), (ma_uint32)10); ++i) {
-        std::cout << snapshot[i] << "\n";
-    }
 
     return 0;
 }
