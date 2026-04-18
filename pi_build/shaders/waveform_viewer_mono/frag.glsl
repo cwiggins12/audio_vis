5c5ea0c461b616f4f394766cc90c8e47b71c3e43
Cody <codywiggins2112@gmail.com>
2026-04-18 02:45:49 -0600

    pi_build

---
+++ pi_build/shaders/waveform_viewer_mono/frag.glsl
@@ -0,0 +1,38 @@
+//hi :)
+
+vec4 helper(float waveY, vec2 px) {
+    float dist = abs(px.y - waveY);
+    float line = smoothstep(2.0, 0.0, dist);
+
+    vec3 col;
+    if (mouseDown == 1) {
+        col = vec3(1.0);
+    }
+    else {
+        col = vec3(mouseX / W, mouseY / H, 0.5);
+    }
+    return vec4(col * line, 1.0);
+}
+
+void main() {
+    vec2 px = toPx();
+    int x = int(px.x);
+    int iW = int(W);
+
+    float waveY;
+    if (newAudioWindow == 1) {
+        int cutoff = iW - hopSize;
+        if (x >= cutoff) {
+            float s = rawSamples[x - cutoff];
+            waveY = (s * 0.5 + 0.5) * H;
+        }
+        else {
+            waveY = feedbackIn[x + hopSize];
+        }
+    }
+    else {
+        waveY = feedbackIn[x];
+    }
+    feedbackOut[x] = waveY;
+    FragColor = helper(waveY, px);
+}
