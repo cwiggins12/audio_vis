6b9e42fc5802ba87af96772a003939eb3dd43196
cody <codywiggins2112@gmail.com>
2026-05-19 14:32:34 -0500

    shader/doc fix

--- src/shaders/crt_lofi_analyser/frag.glsl
+++ src/shaders/crt_lofi_analyser/frag.glsl
@@ -75,7 +75,7 @@ vec3 addPeakMeters(vec3 color, vec2 px) {
 
 vec3 addChannelNum(vec3 color, vec2 px) {
     float fontSize = 64.0;
-    fontSize = fontSize * (W / 1280.0);
+    fontSize = fontSize * (W / 1920.0);
     vec2  labelOrigin = vec2(100.0, H - 60.0 - fontSize);
 	//if (px.y < labelOrigin.y || px.y > labelOrigin.y + fontSize) return color;
     int   ch0 = 48; // '0'
@@ -116,7 +116,7 @@ vec3 addScanning(vec3 color, vec2 px) {
     if (cycle > 1.2) return color;
 
     float fontSize = 32.0;
-    fontSize = fontSize * (W / 1280.0);
+    fontSize = fontSize * (W / 1920.0);
     int totalChars = 11;
 
     float totalW = float(totalChars) * fontSize;
